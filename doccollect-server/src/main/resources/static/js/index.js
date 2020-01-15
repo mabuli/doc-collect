c1ecf4c8975226bbcdc2c8daaffaebe85a01e1e7
wuyaoyao0828 <1003465335@qq.com>
2020-01-15 10:10:43 +0800

    修改数据可视化地址

--- doccollect-server/src/main/resources/static/js/index.js
+++ doccollect-server/src/main/resources/static/js/index.js
@@ -99,19 +99,19 @@ var vm = new Vue({
         getUser : function() {
             $.getJSON("sys/user/info?_" + $.now(), function(r) {
                 vm.user = r.user;
-                var url = "http://10.217.17.116:12100/webroot/decision/login/cross/domain" + "?fine_username=" + vm.user.username + "&fine_password=xmgladmin&validity=" + -1;
                 $.ajax({
-                    url: url,//单点登录的管理平台报表服务器
+                    url: 'http://10.217.17.116:37799/webroot/decision/login',//单点登录的管理平台报表服务器
+                    data: {'fine_username': vm.user.username, 'fine_password':'xmgladmin', 'validity': -1},
                     timeout: 5000,//超时时间（单位：毫秒）
                     dataType:"jsonp",//跨域采用jsonp方式
                     jsonp:"callback",
-                success: function (res) {
-                    console.log(res);
-                    if (res.errorCode) {
-                        window.alert(res.errorMsg);
-                    }else {
-                    }
-                },
+                    success: function (res) {
+                        console.log(res);
+                        if (res.errorCode) {
+                            window.alert(res.errorMsg);
+                        }else {
+                        }
+                    },
                     error: function () {
                         alert("超时或服务器其他错误");// 登录失败（超时或服务器其他错误） 
                     }
