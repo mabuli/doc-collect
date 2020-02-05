5c076e28c61ccf5aac691e6cb8ec7a802c1d7b46
kuibobo <kuibobo@gmail.com>
2020-02-05 20:27:13 +0800

    修改搜索条件

--- doccollect-server/src/main/resources/static/js/module/query/popu.js
+++ doccollect-server/src/main/resources/static/js/module/query/popu.js
@@ -5,7 +5,7 @@ var vm = new Vue({
     query: {
       housAddr: '',
       currAddr: '',
-      hltySituCd: '0',
+      hltySituCd: null,
       start: 1,
       count: 10
     },
@@ -31,26 +31,29 @@ var vm = new Vue({
   },
   methods: {
     handleQuery() {
-      this.$refs['queryForm'].validate((valid) => {
-        if (valid) {
-          let url = 'https://10.217.17.110:8243/query/v1.0/querySixtyPopusByNameOrNum?1=1'
-            + (this.query.housAddr == '' ? '' : '&housAddr=' + this.query.housAddr)
-            + (this.query.currAddr == '' ? '' : '&currAddr=' + this.query.currAddr)
-            + (this.query.hltySituCd == '' ? '' : '&hltySituCd=' + this.query.hltySituCd)
-            + (this.query.start == '' ? '' : '&start=' + this.query.start)
-            + (this.query.count == '' ? '' : '&count=' + this.query.count);
-          console.info('-- handleQuery --', url)
-          vm.list = []
-          $.ajax({
-            type: "GET",
-            url: baseURL + '/proxy/get',
-            data: {url: url},
-            dataType: 'json',
-            success: function (resp) {
-              console.info(resp.data)
-              vm.list = resp.data.Pops.pop ? resp.data.Pops.pop : []
-            }
-          });
+      if (this.query.housAddr == '' && this.query.currAddr == '') {
+        this.$message({
+          message: '户口地址 和 现地址 至少输入一个',
+          type: 'error'
+        });
+        return;
+      }
+      let url = 'https://10.217.17.110:8243/query/v1.0/querySixtyPopusByNameOrNum?1=1'
+        + (this.query.housAddr == '' ? '' : '&housAddr=' + this.query.housAddr)
+        + (this.query.currAddr == '' ? '' : '&currAddr=' + this.query.currAddr)
+        + (this.query.hltySituCd == '' ? '' : '&hltySituCd=' + this.query.hltySituCd)
+        + (this.query.start == '' ? '' : '&start=' + this.query.start)
+        + (this.query.count == '' ? '' : '&count=' + this.query.count);
+      console.info('-- handleQuery --', url)
+      vm.list = []
+      $.ajax({
+        type: "GET",
+        url: baseURL + '/proxy/get',
+        data: {url: url},
+        dataType: 'json',
+        success: function (resp) {
+          console.info(resp.data)
+          vm.list = resp.data.Pops.pop ? resp.data.Pops.pop : []
         }
       });
     }
